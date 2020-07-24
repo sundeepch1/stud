00c15924c405488ead218f7a6416f7382aec21f4
sundeepch1 <sundeep.chaurasiya@gmail.com>
2020-07-24 16:19:15 +0530

    d

---
+++ ads/SimpleProgram/src/com/number/FactorialRecursive.java
@@ -0,0 +1,15 @@
+package com.number;
+
+public class FactorialRecursive {
+
+	public static int factorial(int n) {
+		if (n == 1 || n==0) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+	}
+}
---
+++ oops/oops/src/com/overloading/OverloadingClass.java
@@ -0,0 +1,19 @@
+package com.overloading;
+
+class OverloadingParent{
+	
+}
+
+class OverloadingChild {
+	public int hello() {
+		return 1;
+	}
+}
+
+public class OverloadingClass {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
